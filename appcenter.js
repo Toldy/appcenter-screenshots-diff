9c22183b139e87a7874480fcfdd2a528cda9fdb0
Julien Colin <juliencolin54@gmail.com>
2021-01-08 15:29:00 +0100

    Step 6 - AppCenter Step 2 - LogIn

--- appcenter.js
+++ appcenter.js
@@ -7,12 +7,37 @@ function delay(time) {
     });
 }
 
+async function logInWithGoogle(page) {
+    const navigationPromise = page.waitForNavigation()
+
+    await page.waitForSelector('a[icon="google"]')
+    await page.click('a[icon="google"]')
+    await page.screenshot({ path: 'after.png' })
+
+    await navigationPromise
+    await page.waitForSelector('input[type="email"]')
+    await page.type('input[type="email"]', process.env.GOOGLE_USER)
+    await page.click('#identifierNext')
+
+    await page.waitForSelector('input[type="password"]', { visible: true })
+    await page.type('input[type="password"]', process.env.GOOGLE_PWD)
+
+    await page.waitForSelector('#passwordNext', { visible: true })
+    await page.click('#passwordNext')
+
+    await navigationPromise
+
+    await delay(4000)
+}
+
 (async () => {
     const browser = await puppeteer.launch({ headless: false })
     const page = await browser.newPage()
 
     await page.setViewport({ width: 1280, height: 800 })
     await page.goto('https://appcenter.ms/sign-in')
+
+    await logInWithGoogle(page)
 
     // await browser.close()
 })();
