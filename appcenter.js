4c3197defb35915f6442d1c693c53083567b828e
Julien Colin <juliencolin54@gmail.com>
2021-01-08 14:49:23 +0100

    Step 2 - The hidden side

--- appcenter.js
+++ appcenter.js
@@ -1,9 +1,17 @@
 const puppeteer = require('puppeteer');
 
+function delay(time) {
+    return new Promise(function (resolve) {
+        setTimeout(resolve, time)
+    });
+}
+
 (async () => {
-    const browser = await puppeteer.launch();
+    const browser = await puppeteer.launch({ headless: false });
     const page = await browser.newPage();
     await page.goto('https://example.com');
+
+    await delay(2000);
 
     await page.screenshot({ path: 'example.png' });
 
